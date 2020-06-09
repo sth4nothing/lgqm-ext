be52506697e3daaf40a454f4435e98f95f81fc74
sth4nothing <892223922@qq.com>
2020-06-09 12:41:39 +0800

    弹出窗口新增跳转到论坛的按钮

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,11 @@
 var interval = document.getElementById('interval')
 var span = document.getElementById('span')
 var toggle = document.getElementById('toggle')
+var go = document.getElementById('go')
 window.onload = function () {
     if (localStorage.interval !== undefined) {
         interval.value = localStorage.interval
+        span.textContent = localStorage.interval + 's'
     }
     if (localStorage.host !== undefined) {
         toggle.checked = localStorage.host == 'lgqm.gq'
@@ -14,5 +16,8 @@ window.onload = function () {
     }
     toggle.onchange = function () {
         localStorage.host = toggle.checked ? 'lgqm.gq' : 'lgqm.top'
+    }
+    go.onclick = function () {
+        window.open('https://www.' + localStorage.host, '_blank')
     }
 }
